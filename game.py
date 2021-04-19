6de53d05715f194736e5250507284b05ab281af6
unknown <mpeterson2004@gmail.com>
2021-04-19 14:29:54 -0600

    added logic to determine how many games will be played

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from human import Human
 
 class RockPaper:
     def __init__(self):
+        self.games_to_play = 3
         print("Press 1 for single player:")
         print("Press 2 for head to head play")
         print("Press 3 to watch computers duel")
@@ -14,12 +15,32 @@ class RockPaper:
                     print("oops we need a number between 1 and 3 please try again")
                 else:
                     break
-            except:
+            except ValueError:
                 print("oops we need a number between 1 and 3 please try again")
         if choice == 1:
-            pass
+            self.player_one = Human()
+            self.player_two = Computer()
+            self.game_set()
+
         elif choice == 2:
-            pass
+            self.player_one = Human()
+            self.player_two = Human()
+            self.game_set()
         else:
-            pass
-        #todo create class and methods
+            self.player_one = Computer()
+            self.player_two = Computer()
+            self.game_set()
+
+    def game_set(self):
+        while True:
+            try:
+                print("How many games do you wish to play")
+                game_choice = int(input("please enter a number 3 or larger (it must be an odd number)"))
+
+                if game_choice % 2 == 0 or game_choice < 3:
+                    print("Oops we need and odd number greater and 3 lets try again")
+                else:
+                    self.games_to_play = game_choice
+                    break
+            except ValueError:
+                print("Oops we need and odd number greater and 3 lets try again")
